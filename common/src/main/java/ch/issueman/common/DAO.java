03da9129bd7559393b762b321362d9e67b90eb55
Janik von Rotz <login@janikvonrotz.ch>
2015-04-18 20:39:42 +0200

    DAO exception

--- common/src/main/java/ch/issueman/common/DAO.java
+++ common/src/main/java/ch/issueman/common/DAO.java
@@ -15,10 +15,10 @@ import java.util.List;
  */
 public interface DAO<T, Id extends Serializable> {
 	
-	public void persist(T t);
-	public T getById(Id id);
-	public List<T> getAll();
-	public void update(T t);
-	public void delete(T t);
-	public void deleteAll();
+	public void persist(T t) throws Exception;
+	public T getById(Id id) throws Exception;
+	public List<T> getAll() throws Exception;
+	public void update(T t) throws Exception;
+	public void delete(T t) throws Exception;
+	public void deleteAll() throws Exception;
 }
