329eeb570afeb3288a6fb35e24cb65e22c5dfc68
erwinwilli <erwinwilli@windowslive.com>
2015-04-19 22:02:47 +0200

    Anpassung Sachbearbeiter
    Anpassung  Sachbearbeiter nach Instruktion von Janik

--- common/src/main/java/ch/issueman/common/Sachbearbeiter.java
+++ common/src/main/java/ch/issueman/common/Sachbearbeiter.java
@@ -4,8 +4,6 @@
 package ch.issueman.common;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -21,26 +19,16 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class Sachbearbeiter extends Person {
-
-	@NotNull
-	private Person nachname;
-	@NotNull
-	private Person vorname;
-	@NotNull
-	private Person email;
 	
 	public Sachbearbeiter(){}
 	
 	public Sachbearbeiter(Person nachname, Person vorname, Person email){
 		super();
-		this.nachname = nachname;
-		this.vorname = vorname;
-		this.email = email;
 	}
 
 	@Override
 	public String getDisplayName() {
-		return this.nachname + " " + this.vorname;
+		return Person.class.getName();
 	}
 
 }
