23036d1e482c0ebb6302d047678657b7fa90b9ec
Patrick Elsener <psoleh@hotmail.com>
2015-04-21 20:17:36 +0200

    Attribute aus Kontakt entfernt

--- common/src/main/java/ch/issueman/common/Kontakt.java
+++ common/src/main/java/ch/issueman/common/Kontakt.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -21,37 +20,24 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Kontakt extends Person {
-	
+public abstract class Kontakt extends Person {
+
 	@Id
 	@GeneratedValue
 	private int id;
 	@OneToOne
 	private List<Projekt> projekte;
-	@NotNull
-	private Person nachname;
-	@NotNull
-	private Person vorname;
-	@NotNull
-	private Person email;
 	@ManyToOne
 	private Subunternehmen subunternehmen;
-	
-	
-	public Kontakt(){}
-	
-	public Kontakt(Person nachname, Person vorname, Person email, Subunternehmen subunternehmen, List<Projekt> projekte){
-		super();
-		this.nachname = nachname;
-		this.vorname = vorname;
-		this.email = email;
-		this.subunternehmen = subunternehmen;
-		this.projekte = projekte;
+
+	public Kontakt() {
 	}
 
-	@Override
-	public String getDisplayName() {
-		return this.nachname + " " + this.vorname;
+	public Kontakt(Person nachname, Person vorname, Person email,
+			Subunternehmen subunternehmen, List<Projekt> projekte) {
+		super();
+		this.subunternehmen = subunternehmen;
+		this.projekte = projekte;
 	}
 
 	@Override
