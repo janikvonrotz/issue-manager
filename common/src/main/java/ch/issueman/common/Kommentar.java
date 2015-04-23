8ada6c474e7fddb6cee79767b662f2b81813682c
Janik von Rotz <login@janikvonrotz.ch>
2015-04-23 12:46:51 +0200

    merged calendarfix

--- common/src/main/java/ch/issueman/common/Kommentar.java
+++ common/src/main/java/ch/issueman/common/Kommentar.java
@@ -1,7 +1,6 @@
 package ch.issueman.common;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import javax.persistence.Entity;
--- common/src/main/java/ch/issueman/common/Projekt.java
+++ common/src/main/java/ch/issueman/common/Projekt.java
@@ -1,7 +1,6 @@
 package ch.issueman.common;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
