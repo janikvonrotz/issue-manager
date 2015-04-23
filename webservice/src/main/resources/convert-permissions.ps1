11c10c505710c0d3d9e0eef67b2b06d4eeb89e4d
a2van <aathavan.th@gmail.com>
2015-04-23 16:24:53 +0200

    added permissions

---
+++ webservice/src/main/resources/convert-permissions.ps1
@@ -0,0 +1,13 @@
+$Data = Import-Csv .\permissions.csv -Delimiter ";"
+$Data = $Data | Group-Object -property Objekt | foreach-Object{ 
+   
+   ('"' + $_.Name + '":{')
+   ($_.Group | Group-Object -property Berechtigung | foreach-Object{ 
+
+        $Array = $_.Group | Group-Object -property Rolle | foreach-Object{'"' + $_.Name + '"'} 
+
+        ('"' + $_.Name + '":[' + ($Array -join ",") + ']')
+   }) -join ","
+   ('},')
+} 
+$Data
