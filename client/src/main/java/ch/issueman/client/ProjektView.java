6595b1161a06df333fd1abe1136855e61f6fa5bd
Janik von Rotz <login@janikvonrotz.ch>
2015-05-03 19:53:05 +0200

    fixed filterable

--- client/src/main/java/ch/issueman/client/ProjektView.java
+++ client/src/main/java/ch/issueman/client/ProjektView.java
@@ -6,9 +6,7 @@ import java.util.ResourceBundle;
 import ch.issueman.common.Login;
 import ch.issueman.common.Projekt;
 import ch.issueman.common.Sachbearbeiter;
-
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
@@ -20,11 +18,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ProjektView implements Initializable {
 
-	private static Controller<Projekt, Integer> controller = new Controller<Projekt, Integer>(
-			Projekt.class);
+	private static Controller<Projekt, Integer> controller = new Controller<Projekt, Integer>(Projekt.class);
 
-	private ObservableList<Projekt> masterData = FXCollections
-			.observableArrayList();
+	private FilteredList<Projekt> filteredData = new FilteredList<Projekt>(FXCollections.observableArrayList(),	p -> true);
 
 	private Projekt projekt;
 
@@ -58,71 +54,43 @@ public class ProjektView implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO entfernen
-		Context.setLogin(new Login(new Sachbearbeiter("", "", "sb@im.ch"), "1",
-				null));
+		Context.setLogin(new Login(new Sachbearbeiter("", "", "sb@im.ch"), "1", null));
 		Context.login();
 
-		tcId.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>(
-				"id"));
-		tcTitel.setCellValueFactory(new PropertyValueFactory<Projekt, String>(
-				"title"));
-		tcProjekttyp
-				.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>(
-						"projekttyp_id"));
-		tcArbeitstyp
-				.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>(
-						"arbeitstyp_id"));
-		tcBauleiter
-				.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>(
-						"bauleiter_id"));
-		tcBauherr
-				.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>(
-						"bauherr_id"));
-		tcEnddatum
-				.setCellValueFactory(new PropertyValueFactory<Projekt, String>(
-						"ende"));
+		tcId.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>("id"));
+		tcTitel.setCellValueFactory(new PropertyValueFactory<Projekt, String>("title"));
+		tcProjekttyp.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>("projekttyp"));
+		tcArbeitstyp.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>("arbeitstyp"));
+		tcBauleiter.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>("bauleiter"));
+		tcBauherr.setCellValueFactory(new PropertyValueFactory<Projekt, Integer>("bauherr"));
+		tcEnddatum.setCellValueFactory(new PropertyValueFactory<Projekt, String>("ende"));	
 
-		// 1. Wrap the ObservableList in a FilteredList (initially display all
-		// data).
-		FilteredList<Projekt> filteredData = new FilteredList<>(masterData,
-				p -> true);
-
-		// 2. Set the filter Predicate whenever the filter changes.
 		txFilter.textProperty().addListener(
 				(observable, oldValue, newValue) -> {
-					filteredData.setPredicate(person -> {
-						// If filter text is empty, display all persons.
+					filteredData.setPredicate(t -> {
+						
 							if (newValue == null || newValue.isEmpty()) {
 								return true;
 							}
 
-							// Compare name with filter text.
 							String lowerCaseFilter = newValue.toLowerCase();
 
-							if (projekt.getTitle().toLowerCase()
-									.indexOf(lowerCaseFilter) != -1) {
-								return true; // Filter matches first name.
+							if (t.getTitle().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+								return true; 
 							}
-							return false; // Does not match.
+							return false;
 						});
-				});
-
-		// 3. Wrap the FilteredList in a SortedList.
-		SortedList<Projekt> sortedData = new SortedList<>(filteredData);
-
-		// 4. Bind the SortedList comparator to the TableView comparator.
-		// Otherwise, sorting the TableView would have no effect.
-		sortedData.comparatorProperty().bind(tvData.comparatorProperty());
-
-		// 5. Add sorted (and filtered) data to the table.
-		tvData.setItems(sortedData);
+				});		
 
 		Refresh();
 	}
 
 	public void Refresh() {
 		try {
-			tvData.setItems(FXCollections.observableArrayList(controller.getAll()));
+			filteredData = new FilteredList<Projekt>(FXCollections.observableArrayList(controller.getAll()),	p -> true);
+			SortedList<Projekt> sortedData = new SortedList<Projekt>(filteredData);
+			sortedData.comparatorProperty().bind(tvData.comparatorProperty());
+			tvData.setItems(sortedData);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
