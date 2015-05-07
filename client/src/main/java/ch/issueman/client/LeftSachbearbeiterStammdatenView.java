755fdbbabb664fc1b8fcf4dc127933c71e11807c
Patrick Elsener <psoleh@hotmail.com>
2015-05-07 12:28:45 +0200

    SplitMenu in Login, Bauleiter in TopPane, Sachbearbeiter in LeftPane

---
+++ client/src/main/java/ch/issueman/client/LeftSachbearbeiterStammdatenView.java
@@ -0,0 +1,14 @@
+package ch.issueman.client;
+
+
+/**
+ * View for Sachbearbeiter in LeftPane
+ * 
+ * @author Patrick Elsener
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+public class LeftSachbearbeiterStammdatenView {
+
+}
