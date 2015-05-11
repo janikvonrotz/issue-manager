7d5c560547dd619e84425ff97234c6b0aa6eca0e
Patrick Elsener <psoleh@hotmail.com>
2015-05-11 14:45:52 +0200

    Error Box & ComboBox

--- client/src/main/java/ch/issueman/client/MainView.java
+++ client/src/main/java/ch/issueman/client/MainView.java
@@ -1,14 +1,23 @@
 package ch.issueman.client;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 
 /**
  * Main view controller that holds the border pane and handles the view navigation.
@@ -35,6 +44,11 @@ public class MainView implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		root = bpMain;
+		try{
+			int i = 1/0;
+		}catch(Exception e){
+			showError(e);
+		}
 		showCenterView("Person");
 		showTopView("TopBauleiter");
 		showLeftView("LeftSachbearbeiterStammdaten");
@@ -111,4 +125,38 @@ public class MainView implements Initializable {
 		}
 		return loader.getController();
 	}
+	
+	public static void showError(Exception e) {
+//		Alert alert = new Alert(AlertType.ERROR);
+//		alert.setTitle("Error");
+//		alert.setHeaderText("Exception occurred.");
+//		alert.setContentText(e.getMessage());
+//			
+//		StringWriter sw = new StringWriter();
+//		PrintWriter pw = new PrintWriter(sw);
+//		e.printStackTrace(pw);
+//		String exceptionText = sw.toString();
+//
+//		Label label = new Label("The exception stacktrace was:");
+//
+//		TextArea textArea = new TextArea(exceptionText);
+//		textArea.setEditable(false);
+//		textArea.setWrapText(true);
+//
+//		textArea.setMaxWidth(Double.MAX_VALUE);
+//		textArea.setMaxHeight(Double.MAX_VALUE);
+//		GridPane.setVgrow(textArea, Priority.ALWAYS);
+//		GridPane.setHgrow(textArea, Priority.ALWAYS);
+//
+//		GridPane expContent = new GridPane();
+//		expContent.setMaxWidth(Double.MAX_VALUE);
+//		expContent.add(label, 0, 0);
+//		expContent.add(textArea, 0, 1);
+//
+//		alert.getDialogPane().setExpandableContent(expContent);
+//
+//		alert.showAndWait();
+		e.printStackTrace();
+	}
+	
 }
