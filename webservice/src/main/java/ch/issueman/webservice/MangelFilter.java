5a98eb04686f252410d684a62da8b5c1d331ee95
erwinwilli <erwinwilli@windowslive.com>
2015-05-11 14:47:29 +0200

    Update MangelFilter

--- webservice/src/main/java/ch/issueman/webservice/MangelFilter.java
+++ webservice/src/main/java/ch/issueman/webservice/MangelFilter.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import ch.issueman.common.Mangel;
 import ch.issueman.common.Kontakt;
-import ch.issueman.common.Subunternehmen;
 
 /**
  * Filter Projects by mangel and login roles.
@@ -20,52 +19,28 @@ public class MangelFilter extends TypeFilter<Mangel, Integer> {
 		super(Mangel.class);
 	}
 
-//	@Override
-//	public List<Mangel> getAll() {
-//		if(this.ifUserHasRole("Sachbearbeiter")){
-//			return this.getController().getAll();
-//		}else if(Mangel.getSubunternehmen() == Kontakt.getSubunternehmen()){
-//			return this.getController().getAll().stream()
-//					.filter(Mangel -> Mangel.equals(this.Mangel))
-//					.collect(Collectors.toList());
-//		}
-//	}
-	
-	/* (non-Javadoc)
-	 * @see ch.issueman.webservice.TypeFilter#getById(java.io.Serializable)
-	 */
 	@Override
-	public Mangel getById(Integer id) throws Exception {
-		return super.getById(id);
-	}
-
-	/* (non-Javadoc)
-	 * @see ch.issueman.webservice.TypeFilter#delete(java.lang.Object)
-	 */
-	@Override
-	public void delete(Mangel t) throws Exception {
-		if(this.ifUserHasRole("Sachbearbeiter")){
-			super.delete(t);
+	public List<Mangel> getAll() {
+		if(this.getLogin().getPerson() instanceof Kontakt){
+			return this.getController().getAll().stream()
+					.filter(m -> m.getSubunternehmen().equals(((Kontakt) this.getLogin().getPerson()).getSubunternehmen()))
+					.collect(Collectors.toList());
 		}else{
-			throw new Exception("Not allowed to delete Mangel.");
+			return this.getController().getAll();
 		}
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see ch.issueman.webservice.TypeFilter#getAllByProperty(java.lang.String, java.util.List)
 	 */
-//	@Override
-//	public List<Mangel> getAllByProperty(String propertyname,
-//			List<String> propertyvalues) throws Exception {
-//		if(this.ifUserHasRole("Sachbearbeiter")){
-//			return this.getController().getAllByProperty(propertyname, propertyvalues);
-//		}else if(this.getMangel().){
-//			super.getAllByProperty(propertyname, propertyvalues);
-//		}else{
-//			throw new Exception("Not allowed to update Login.")
-//		}
-//		return super.getAllByProperty(propertyname, propertyvalues);
-//	}
-
-
+	@Override
+	public List<Mangel> getAllByProperty(String propertyname,List<String> propertyvalues) throws Exception {
+		if(this.getLogin().getPerson() instanceof Kontakt){
+			return this.getController().getAllByProperty(propertyname, propertyvalues).stream()
+					.filter(m -> m.getSubunternehmen().equals(((Kontakt) this.getLogin().getPerson()).getSubunternehmen()))
+					.collect(Collectors.toList());
+		}else{
+			return this.getController().getAllByProperty(propertyname, propertyvalues);
+		}
+	}
 }
