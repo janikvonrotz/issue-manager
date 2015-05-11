641f31873aecb34889a92b38be90b1636fe459db
erwinwilli <erwinwilli@windowslive.com>
2015-05-11 21:04:30 +0200

    Update ProjektFilter
    Hinzufügen der Funktion getAllByProperty --> Test erfolgreich

--- webservice/src/main/java/ch/issueman/webservice/ProjektFilter.java
+++ webservice/src/main/java/ch/issueman/webservice/ProjektFilter.java
@@ -42,4 +42,25 @@ public class ProjektFilter extends TypeFilter<Projekt, Integer> {
 					.collect(Collectors.toList());
 		}
 	}
+
+
+	/* (non-Javadoc)
+	 * @see ch.issueman.webservice.TypeFilter#getAllByProperty(java.lang.String, java.util.List)
+	 */
+	@Override
+	public List<Projekt> getAllByProperty(String propertyname,
+			List<String> propertyvalues) throws Exception {
+		
+		if(this.ifUserHasRole("Sachbearbeiter")){
+			return this.getController().getAllByProperty(propertyname, propertyvalues);
+		}else if(this.getLogin().getPerson() instanceof Kontakt){
+			return this.getController().getAllByProperty(propertyname, propertyvalues).stream()
+					.filter(p -> p.getArchiviert() == false && ((Kontakt) this.getLogin().getPerson()).getProjekte().contains(p))
+					.collect(Collectors.toList());	
+		}else{
+			return this.getController().getAllByProperty(propertyname, propertyvalues).stream()
+					.filter(p -> p.getArchiviert() == false)
+					.collect(Collectors.toList());
+		}
+	}
 }
