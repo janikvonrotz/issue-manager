22daa66cef863b99e79bf1994d84e3d08de6c8bc
Patrick Elsener <psoleh@hotmail.com>
2015-05-13 14:03:23 +0200

    LeftMangel ListView mit Projekten

--- client/src/main/java/ch/issueman/client/LeftMangelView.java
+++ client/src/main/java/ch/issueman/client/LeftMangelView.java
@@ -3,6 +3,16 @@ package ch.issueman.client;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+import ch.issueman.common.Projekt;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+
 /**
  * Mangel filter view for left pane.
  * 
@@ -13,6 +23,11 @@ import java.util.ResourceBundle;
 public class LeftMangelView implements ViewableDetail<Object> {
 
 	// Projekte Controller deklarieren
+	private static Controller<Projekt, Integer> projektcontroller = new Controller<Projekt, Integer>(Projekt.class);
+	
+	
+	@FXML
+	private ListView<Projekt> lvProjekt;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -24,6 +39,35 @@ public class LeftMangelView implements ViewableDetail<Object> {
 	@Override
 	public void Refresh() {
 		// List View mit Projekten laden
+		
+		try {
+			ObservableList<Projekt> lvData = FXCollections.observableArrayList();
+			lvData.addAll(projektcontroller.getAll());
+			lvProjekt.setItems(lvData);
+			lvProjekt.setCellFactory((list) -> {
+			    return new ListCell<Projekt>() {
+			        @Override
+			        protected void updateItem(Projekt item, boolean empty) {
+			            super.updateItem(item, empty);
+
+			            if (item == null || empty) {
+			                setText(null);
+			            } else {
+			                setText(item.getTitle());
+//			                System.out.println(item.getTitle());
+			            }
+			        }
+			    };
+			});
+			
+			lvProjekt.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			    System.out.println("ListView Selection Changed (selected: " + newValue.toString() + ")");
+			});
+		} catch (Exception e) {			
+			MainView.showError(e);
+		}
+		
+
 
 	}
 
@@ -36,4 +80,6 @@ public class LeftMangelView implements ViewableDetail<Object> {
 	}
 	
 	// egänzen list view click methode mit aufruf der der center Mangel View mit Projekt als parameter
+	
+	
 }
