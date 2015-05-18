dfb5dd31d9f18460e3a3a4351f31f8d35f770306
sandroklarer <s.klarer@outlook.com>
2015-05-18 17:48:30 +0200

    yolo

---
+++ Create-WordFromSource.ps1
@@ -0,0 +1,37 @@
+﻿$path = "C:\Users\S\Documents\GitHub\issue-manager\export.docx"
+$PDFpath = "C:\Users\S\Documents\GitHub\issue-manager\export.docx"
+
+# Required Word Variables
+$wdExportFormatPDF = 17
+$wdDoNotSaveChanges = 0
+
+# Create a hidden Word window
+$word = New-Object -ComObject word.application
+$word.visible = $false
+
+# Add a Word document
+$doc = $word.documents.add()
+$content = @()
+$selection = $word.selection
+
+Get-ChildItem -Recurse -Filter "*.java" | ForEach-Object{
+    $Selection.Style = 'Überschrift 1'
+    $Selection.TypeText($_.BaseName)
+    $Selection.TypeParagraph()
+    $selection.typeText($(Get-Content $_.FullName -Raw) + "`v")
+    $selection.InsertBreak(7)
+}
+
+
+
+$doc.saveas($path, $saveFormat::wdFormatDocument)
+
+<#
+$doc.ExportAsFixedFormat($PDFpath,$wdExportFormatPDF)
+#>
+
+$doc.close($wdDoNotSaveChanges)
+$word.Quit()
+
+
+
