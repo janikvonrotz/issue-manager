1b67be134e92a35ebac753d477613ead4760ac21
Janik von Rotz <login@janikvonrotz.ch>
2015-05-18 13:51:29 +0200

    updated imports
    do not remove

--- client/src/main/java/ch/issueman/client/MainView.java
+++ client/src/main/java/ch/issueman/client/MainView.java
@@ -2,6 +2,8 @@ package ch.issueman.client;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +15,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
