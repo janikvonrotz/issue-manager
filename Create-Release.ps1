62517ca1a2d553184613ec7a49eb16e44f898d74
Janik von Rotz <login@janikvonrotz.ch>
2015-05-19 11:46:55 +0200

    release script

---
+++ Create-Release.ps1
@@ -0,0 +1,23 @@
+git checkout master
+gradle shadowjar
+gradle assemble
+
+function create-7zip([String] $aDirectory, [String] $aZipfile){
+    [string]$pathToZipExe = "C:\Program Files\7-zip\7z.exe";
+    [Array]$arguments = "a", "-tzip", "$aZipfile", "$aDirectory";
+    & $pathToZipExe $arguments;
+}
+
+create-7zip ".\lib\*"  "mysql.zip"
+
+git checkout postgres
+gradle shadowjar
+gradle assemble
+
+function create-7zip([String] $aDirectory, [String] $aZipfile){
+    [string]$pathToZipExe = "C:\Program Files\7-zip\7z.exe";
+    [Array]$arguments = "a", "-tzip", "$aZipfile", "$aDirectory";
+    & $pathToZipExe $arguments;
+}
+
+create-7zip ".\lib\*"  "postgres.zip"
